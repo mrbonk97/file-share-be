963fbf3ac437bdfd937c03da2b792e93bbfa08d6
mrbonk97 <mrbonk97@gmail.com>
2024-03-14 13:20:04 +0900

    first commit

---
+++ src/main/java/org/mrbonk97/fileshareserver/FileShareServerApplication.java
@@ -0,0 +1,13 @@
+package org.mrbonk97.fileshareserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileShareServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileShareServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/mrbonk97/fileshareserver/FileShareServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mrbonk97.fileshareserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileShareServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
